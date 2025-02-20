24d71f2ee3f14ce70a581e67bdbbbb96a06c1c28
최우석 <56614719+chldntjr1321@users.noreply.github.com>
2025-02-20 17:33:29 +0900

    Click and Draw

--- app.js
+++ app.js
@@ -2,16 +2,40 @@ const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
+ctx.lineWidth = 2;
 
-ctx.fillRect(210 - 40, 200 - 30, 15, 100);
-ctx.fillRect(350 - 40, 200 - 30, 15, 100);
-ctx.fillRect(260 - 40, 200 - 30, 60, 200);
+const colors = [
+  "#55efc4",
+  "#81ecec",
+  "#74b9ff",
+  "#dfe6e9",
+  "#dfe6e9",
+  "#ffeaa7",
+  "#ff7675",
+  "#d63031",
+  "#fdcb6e",
+];
 
-ctx.arc(250, 100, 50, 0, 2 * Math.PI);
-ctx.fill();
+let x_coord = 0;
+let y_coord = 0;
 
-ctx.beginPath();
-ctx.fillStyle = "white";
-ctx.arc(260 + 10, 80, 8, Math.PI, 2 * Math.PI);
-ctx.arc(220 + 10, 80, 8, Math.PI, 2 * Math.PI);
-ctx.fill();
+function onClick(event) {
+  ctx.beginPath();
+  ctx.moveTo(x_coord, y_coord);
+  ctx.strokeStyle = colors[Math.floor(Math.random() * colors.length)];
+  ctx.lineTo(event.offsetX, event.offsetY);
+  ctx.stroke();
+}
+
+function cursor_move(event) {
+  x_coord = event.offsetX;
+  y_coord = event.offsetY;
+  ctx.beginPath();
+  ctx.arc(x_coord, y_coord, 5, 0, 2 * Math.PI);
+  ctx.fillStyle = colors[Math.floor(Math.random() * colors.length)];
+  ctx.fill();
+  console.log("d");
+}
+
+canvas.addEventListener("mousemove", onClick);
+canvas.addEventListener("click", cursor_move);
